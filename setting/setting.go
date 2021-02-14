7886d319735b20895a8a97d10da6a8259255d90a
liqiqiorz <1099462011@qq.com>
2021-02-14 11:36:43 +0800

    feat:vote for topic

--- setting/setting.go
+++ setting/setting.go
@@ -71,8 +71,10 @@ func Init(filePath string) (err error) {
 	err = viper.ReadInConfig() // 读取配置信息
 	if err != nil {
 		// 读取配置信息失败
+
 		fmt.Printf("viper.ReadInConfig failed, err:%v\n", err)
 		return
+
 	}
 
 	// 把读取到的配置信息反序列化到 Conf 变量中
